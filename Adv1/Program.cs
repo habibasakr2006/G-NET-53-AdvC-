0f4cf6dc8c97722a3807abc3164005e6f7a27f01
habibasakr2006 <habiba.sakr09@gmail.com>
2026-08-16 21:36:21 +0300

    Q3

--- Adv1/Program.cs
+++ Adv1/Program.cs
@@ -33,17 +33,17 @@ namespace Adv1
 
     #region Q3
 
-    public class Pair<TKey, TValue>
-    {
-        public TKey Key { get; set; }
-        public TValue Value { get; set; }
+    //public class Pair<TKey, TValue>
+    //{
+    //    public TKey Key { get; set; }
+    //    public TValue Value { get; set; }
 
-        public Pair(TKey key, TValue value)
-        {
-            Key = key;
-            Value = value;
-        }
-    }
+    //    public Pair(TKey key, TValue value)
+    //    {
+    //        Key = key;
+    //        Value = value;
+    //    }
+    //}
 
     #endregion
 
